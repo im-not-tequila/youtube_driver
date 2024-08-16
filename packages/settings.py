1f78ba8a4aad572618b74222c409d769d86d6e53
im_not_tequila <i.am.tequila1236@gmail.com>
2024-08-16 10:27:24 +0500

    160824

--- packages/settings.py
+++ packages/settings.py
@@ -1,2 +1,12 @@
+import os
+
+
+directory = "./profiles"
+
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+
 class Settings:
     PROFILES_DIRECTORY = 'C:/work/activity_project/youtube_driver/profiles'
